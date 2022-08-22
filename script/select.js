616c0b17d61e5f3afa3f49f84c15198d313b8d71
Abu Sayed <rafsansayed132@gmail.com>
2022-08-22 19:31:48 +0600

    added player select

--- script/select.js
+++ script/select.js
@@ -1,16 +1,23 @@
-document.getElementById('plears-seloctor').addEventListener('click', function(event){
-    if(event.target.innrHTML === 'Select'){
-        
-        const playerName = event.target.parentNode.parentNode.childNodes[4].childNodes[1].innerText;
-        const selectPlayer = document.querySelectorAll('#select li');
-        if(selectPlayer === 5){
-            return alert('5 Player Seleted Done And No More Selected')
-        }
 
-        const playerList = document.getElementById('select');
-        const li = document.createElement('li');
-        li.append(playerName);
-        playerList.append(li)
-        
-    }
-})
+
+document.getElementById("player-container").addEventListener("click", function(event) {
+
+if (event.target.innerText == 'Select') {
+
+const playerName = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+const playerCount = document.querySelectorAll("#players li");
+
+if (playerCount.length > 4) {
+    return alert("You can't select more than five!");
+}
+
+const playerList = document.getElementById("players");
+const li = document.createElement("li");
+li.append(playerName);
+playerList.append(li);
+event.target.setAttribute("disabled", "true");
+
+
+}
+
+});
